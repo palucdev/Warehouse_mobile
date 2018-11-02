2ba4e46bedb9172ee6d4a243d68052c6743ccdec
Dawid Paluchowski <paluchowskid@gmail.com>
2018-11-02 11:08:39 +0100

    Added login form validation

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:validate/validate.dart';
 
 class _LoginData {
   String email = '';
@@ -8,13 +9,12 @@ class _LoginData {
 class LoginScreenState extends State<LoginScreen> {
   BuildContext _ctx;
 
-  final GlobalKey<FormState>_formKey = new GlobalKey<FormState>();
+  final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
+  _LoginData _data = new _LoginData();
 
   @override
   Widget build(BuildContext context) {
-    final Size screenSize = MediaQuery
-        .of(context)
-        .size;
+    final Size screenSize = MediaQuery.of(context).size;
 
     return new Scaffold(
         appBar: new AppBar(
@@ -24,41 +24,39 @@ class LoginScreenState extends State<LoginScreen> {
             padding: new EdgeInsets.all(20.0),
             child: new Form(
                 key: this._formKey,
-                child: new ListView(
-                    children: <Widget>[
-                      new TextFormField(
-                          keyboardType: TextInputType.emailAddress,
-                          decoration: new InputDecoration(
-                              hintText: 'example@domain.com',
-                              labelText: 'E-mail Address'
-                          )
+                child: new ListView(children: <Widget>[
+                  new TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: new InputDecoration(
+                        hintText: 'example@domain.com',
+                        labelText: 'E-mail Address'),
+                    validator: this._validateEmail,
+                    onSaved: (String value) {
+                      this._data.email = value;
+                    },
+                  ),
+                  new TextFormField(
+                      obscureText: true,
+                      decoration: new InputDecoration(
+                          hintText: 'Password',
+                          labelText: 'Enter your password'),
+                      validator: this._validatePassword,
+                      onSaved: (String value) {
+                        this._data.password = value;
+                      }),
+                  new Container(
+                    width: screenSize.width,
+                    child: new RaisedButton(
+                      child: new Text(
+                        'Login',
+                        style: new TextStyle(color: Colors.white),
                       ),
-                      new TextFormField(
-                          obscureText: true,
-                          decoration: new InputDecoration(
-                              hintText: 'Password',
-                              labelText: 'Enter your password'
-                          )
-                      ),
-                      new Container(
-                          width: screenSize.width,
-                          child: new RaisedButton(
-                              child: new Text(
-                                  'Login',
-                                  style: new TextStyle(
-                                    color: Colors.white
-                                  ),
-                              ),
-                            onPressed: _submit,
-                            color: Colors.blue,
-                          ),
-                        margin: new EdgeInsets.only(top: 20.0),
-                      )
-                    ]
-                )
-            )
-        )
-    );
+                      onPressed: _submit,
+                      color: Colors.blue,
+                    ),
+                    margin: new EdgeInsets.only(top: 20.0),
+                  )
+                ]))));
   }
 
   void _submit() {
@@ -70,10 +68,21 @@ class LoginScreenState extends State<LoginScreen> {
     }
   }
 
-  String _isUserEmailValid(String val) {
-    //TODO email validation <3
-    //return error strings for certain validation errors
-    //if everything is ok, return null
+  String _validateEmail(String value) {
+    try {
+      Validate.isEmail(value);
+    } catch (e) {
+      return "Email address must be valid (like example@gmail.com)";
+    }
+
+    return null;
+  }
+
+  String _validatePassword(String value) {
+    if (value.length < 8) {
+      return "Password must be at least 8 characters";
+    }
+
     return null;
   }
 }
