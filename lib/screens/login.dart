afca592093e6a40f198153c373703681aa785372
Dawid Paluchowski <paluchowskid@gmail.com>
2018-11-03 19:05:15 +0100

    Initial OpenID connect token retrieving from google

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,9 +1,13 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:validate/validate.dart';
 import 'package:warehouse_mobile/data/db_helper.dart';
 import 'package:warehouse_mobile/data/rest_ds.dart';
 import 'package:warehouse_mobile/model/user.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:warehouse_mobile/utils/shared_pref_util.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class _LoginData {
   String email = '';
@@ -17,6 +21,12 @@ class LoginScreenState extends State<LoginScreen> {
   _LoginData _data = new _LoginData();
 
   RestDatasource api = new RestDatasource();
+
+  GoogleSignIn _googleSignIn =
+      new GoogleSignIn(signInOption: SignInOption.standard, scopes: ['openid']);
+  GoogleSignInAccount _currentUser;
+  GoogleSignInAuthentication _googleAuth;
+  FirebaseAuth _auth = FirebaseAuth.instance;
 
   @override
   Widget build(BuildContext context) {
@@ -62,6 +72,18 @@ class LoginScreenState extends State<LoginScreen> {
                       color: Colors.blue,
                     ),
                     margin: new EdgeInsets.only(top: 20.0),
+                  ),
+                  new Container(
+                    width: screenSize.width,
+                    child: new RaisedButton(
+                      child: new Text(
+                        'Login with Google',
+                        style: new TextStyle(color: Colors.white),
+                      ),
+                      onPressed: _googleLogin,
+                      color: Colors.primaries[0],
+                    ),
+                    margin: new EdgeInsets.only(top: 20.0),
                   )
                 ]))));
   }
@@ -78,6 +100,20 @@ class LoginScreenState extends State<LoginScreen> {
       this.api.login(this._data.email, this._data.password).then((User user) {
         this.onLoginSuccess(user);
       }).catchError((Object error) => this.onLoginError(error.toString()));
+    }
+  }
+
+  Future<void> _googleLogin() async {
+    try {
+      this._currentUser = await _googleSignIn.signIn();
+      this._googleAuth = await this._currentUser.authentication;
+//      FirebaseUser user = await _auth.signInWithGoogle(
+//          idToken: _googleAuth.accessToken, accessToken: _googleAuth.idToken);
+      print('Google auth: ' + this._googleAuth.accessToken.substring(0, 10));
+//      print('signed in' + user.displayName);
+    } catch (error) {
+      print('Google login error');
+      print(error);
     }
   }
 
@@ -109,9 +145,10 @@ class LoginScreenState extends State<LoginScreen> {
     var db = new DatabaseHelper();
     db.saveUser(user);
 
-    SharedPreferencesUtil.saveString(RestDatasource.TOKEN_KEY, user.token).then((void v) {
-			Navigator.of(_ctx).pushReplacementNamed("/home");
-		});
+    SharedPreferencesUtil.saveString(RestDatasource.TOKEN_KEY, user.token)
+        .then((void v) {
+      Navigator.of(_ctx).pushReplacementNamed("/home");
+    });
   }
 }
 
